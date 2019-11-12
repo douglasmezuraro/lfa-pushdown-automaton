b3a119a24c84c5c2bd5a8b1522b31091129cd026
Douglas Mezuraro <douglasmez@gmail.com>
2019-11-12 17:56:23 -0300

    Ajuste

--- AP/pkg/AP.dpr
+++ AP/pkg/AP.dpr
@@ -4,14 +4,14 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   Helper.FMX in '..\src\Helper\Helper.FMX.pas',
-  Impl.AP in '..\src\Impl\Impl.AP.pas',
+  Impl.PushdownAutomaton in '..\src\Impl\Impl.PushdownAutomaton.pas',
   Impl.Dialogs in '..\src\Impl\Impl.Dialogs.pas',
   Impl.List in '..\src\Impl\Impl.List.pas',
   Impl.Stack in '..\src\Impl\Impl.Stack.pas',
   Impl.Transitions in '..\src\Impl\Impl.Transitions.pas',
   Impl.Types in '..\src\Impl\Impl.Types.pas',
   View.Main in '..\src\View\View.Main.pas' {Main},
-  Impl.AP.Validator in '..\src\Impl\Impl.AP.Validator.pas';
+  Impl.PushdownAutomaton.Validator in '..\src\Impl\Impl.PushdownAutomaton.Validator.pas';
 
 {$R *.res}
 
