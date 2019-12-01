5a9eb40d6134754567f2be642d6d549619d8dc1f
Douglas Mezuraro <douglasmez@gmail.com>
2019-12-01 18:54:45 -0300

    Teste unitário: Exercício 2 da lista de exercícios

--- PushdownAutomaton.Tests/src/Test/Test.Exercises.pas
+++ PushdownAutomaton.Tests/src/Test/Test.Exercises.pas
@@ -15,6 +15,9 @@ type
   published
     // L1 = {(a^n)(b^n)/n>=0}
     procedure ExerciseOne;
+
+    // L2 = {(a^n)(b^m)(c^n)/n>0;m>0}
+    procedure ExerciseTwo;
   end;
 
 implementation
@@ -42,12 +45,38 @@ begin
   FAutomaton.AuxSymbols := ['Z', 'X'];
   FAutomaton.Base := 'Z';
 
-  // Source ; Target ; Symbol ; Pop ; Push
   FAutomaton.Transitions.Add(TTransition.Create('q0', 'q0', 'ʎ', 'Z', 'ʎ'));
   FAutomaton.Transitions.Add(TTransition.Create('q0', 'q1', 'a', 'Z', 'X'));
   FAutomaton.Transitions.Add(TTransition.Create('q1', 'q1', 'a', 'X', 'XX'));
   FAutomaton.Transitions.Add(TTransition.Create('q1', 'q2', 'b', 'X', 'ʎ'));
   FAutomaton.Transitions.Add(TTransition.Create('q2', 'q2', 'b', 'X', 'ʎ'));
+
+  for Word in MustAccept do
+    CheckTrue(FAutomaton.Accept(Word));
+
+  for Word in MustNotAccept do
+    CheckFalse(FAutomaton.Accept(Word));
+end;
+
+procedure TExercisesTest.ExerciseTwo;
+const
+  MustAccept: TArray<TWord> = ['abc', 'abbc', 'abbbc', 'aabcc', 'aabbcc', 'aaabccc', 'aaabbccc'];
+  MustNotAccept: TArray<TWord> = ['ʎ', 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abcc', 'aabc', 'aabccc'];
+var
+  Word: TWord;
+begin
+  FAutomaton.Symbols := ['a', 'b', 'c'];
+  FAutomaton.States := ['q0', 'q1', 'q2', 'q3'];
+  FAutomaton.InitialState := 'q0';
+  FAutomaton.AuxSymbols := ['Z', 'X'];
+  FAutomaton.Base := 'Z';
+
+  FAutomaton.Transitions.Add(TTransition.Create('q0', 'q1', 'a', 'Z', 'X'));
+  FAutomaton.Transitions.Add(TTransition.Create('q1', 'q1', 'a', 'X', 'XX'));
+  FAutomaton.Transitions.Add(TTransition.Create('q1', 'q2', 'b', 'X', 'X'));
+  FAutomaton.Transitions.Add(TTransition.Create('q2', 'q2', 'b', 'X', 'X'));
+  FAutomaton.Transitions.Add(TTransition.Create('q2', 'q3', 'c', 'X', 'ʎ'));
+  FAutomaton.Transitions.Add(TTransition.Create('q3', 'q3', 'c', 'X', 'ʎ'));
 
   for Word in MustAccept do
     CheckTrue(FAutomaton.Accept(Word));
